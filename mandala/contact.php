2e694c9d384f1258fe4c6236aed43cc37f7684ee
Rentaro$ <86515218+akash120123@users.noreply.github.com>
2025-04-04 11:41:04 +0545

    Update contact.php

--- mandala/contact.php
+++ mandala/contact.php
@@ -96,7 +96,7 @@
 
 
     <!-- Google Map js -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBmGmeot5jcjdaJTvfCmQPfzeoG_pABeWo "></script>
+    <script src="#"></script>
     <script src="js/contact-map.js"></script>
     <script>
         // When the window has finished loading create our google map below
